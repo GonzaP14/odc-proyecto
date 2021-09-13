b219550975bd75d4056eed3fd0ffdc6c33e20f50
nico23112001 <62406648+nico23112001@users.noreply.github.com>
2021-09-13 20:07:44 -0300

    Commit 8

--- Proyecto-ODC/convert.c
+++ Proyecto-ODC/convert.c
@@ -2,13 +2,5 @@
 #include <stdlib.h>
 
 int main () {
-    int base = 8;
-    char num [] = "7654";
-
-    char * np = &num ;
-    double * res;
-
-    res = metodoMultiplicacion(np , &base);
-    printf("%lf" , *res);
     return 0;
 }
