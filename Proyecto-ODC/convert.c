b98512b651abc1d59979c9c4cbe527ce2746c9b7
nico23112001 <62406648+nico23112001@users.noreply.github.com>
2021-10-07 14:50:48 -0300

    Pusheo con printf corregido en Partes enteras

--- Proyecto-ODC/convert.c
+++ Proyecto-ODC/convert.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 
 int main () {
+    char numero[5] = "1672";
+    char* pnum = &numero[0];
 
+    short base = 8;
+    short * pb = &base;
+
+    short uno = 1;
+    short * pu = &uno;
+
+    long* resultado = parteEnteraOrigenA10( pnum, pb, pu );
+    free(resultado);
     return 0;
 }
