2887a0370d07a11a31a9b0fee596a35409b80a16
nico23112001 <62406648+nico23112001@users.noreply.github.com>
2021-10-08 21:41:43 -0300

    Commit con correcion

--- Proyecto-ODC/convert.c
+++ Proyecto-ODC/convert.c
@@ -12,6 +12,7 @@ int main (int argc, char ** argv) {
     short * help;
     short * contador;
     short * numeroAsignado;
+    short * cantidadDeParametros;
 
     numeroAsignado = (short *) malloc (sizeof (short));
     baseOrigen = (short *) malloc (sizeof (short));
@@ -19,6 +20,7 @@ int main (int argc, char ** argv) {
     verbose = (short *) malloc (sizeof (short));
     help = (short *) malloc (sizeof (short));
     contador = (short *) malloc (sizeof (short));
+    cantidadDeParametros = (short *) malloc (sizeof (short));
 
     *numeroAsignado = 0;
     *baseOrigen = 0;
@@ -26,27 +28,33 @@ int main (int argc, char ** argv) {
     *verbose = 0;
     *help = 0;
     *contador = 1;
+    *cantidadDeParametros = 0;
 
     if (argc <= 9) {
         while (*contador <= argc ) {
             if (*numeroAsignado == 0 && strcmp (argv [*contador], "-n" ) == 0 ) {
+                *cantidadDeParametros+= 2;
                 *numeroAsignado = 1;
                 numero = argv [*contador + 1];
             }
 
             if (*baseOrigen == 0 && strcmp (argv [*contador], "-s") == 0) {
+                *cantidadDeParametros += 2;
                 *baseOrigen = atoi (argv [*contador + 1]);
             }
 
             if (*baseDestino == 0 && strcmp (argv [*contador], "-d") == 0 ) {
+                *cantidadDeParametros += 2;
                 *baseDestino = atoi (argv [*contador + 1]);
             }
 
             if (*verbose == 0 && strcmp (argv [*contador], "-v") == 0 ) {
+                *cantidadDeParametros += 1;
                 *verbose = 1;
             }
 
             if (*help == 0 && strcmp (argv [*contador], "-h") == 0 ) {
+                *cantidadDeParametros += 1;
                 *help = 1;
             }
             *contador += 1;
@@ -58,29 +66,23 @@ int main (int argc, char ** argv) {
         if (*baseDestino == 0) {
             *baseDestino = 10;
         }
-
+        printf("hola");
         resultado = convertir (numero, baseOrigen, baseDestino, verbose);
 
         printf ("El numero %s en base %hd es igual a %s en base %hd.", numero, *baseOrigen, resultado, *baseDestino);
     }
-
     else if (argc > 9) {
         printf ("Error: La cantidad de argumentos ingresada. ");
     }
-    else {
-        printf ("Se necesita que ingrese un argumento.");
-    }
-
     free (resultado);
     free (baseOrigen);
     free (baseDestino);
     free (verbose);
     free (help);
     free(numeroAsignado);
-
-    while (*contador > 0) {
-        *contador -= 1;
-        free (argv [*contador]);
+    while (*cantidadDeParametros > 0) {
+        *cantidadDeParametros -=1;
+        free (argv [*cantidadDeParametros]);
     }
 
     free (argv);
