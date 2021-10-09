130b08ec04b72a63d01ab97c7115d9f5149ea3e8
nico23112001 <62406648+nico23112001@users.noreply.github.com>
2021-10-09 19:19:01 -0300

    Faltan solo los comentarios

--- Proyecto-ODC/convert.c
+++ Proyecto-ODC/convert.c
@@ -10,11 +10,68 @@ void validarNumero (char * numero, short * check) {
 }
 
 void validarBase (char * base, short * check) {
+    short * contador;
 
+    contador = (short *) malloc (sizeof (short));
+    *contador = 0;
+
+    while (base [*contador] != '\0'){
+        if (base [*contador] >= '0' && base [*contador] <= '9'){
+            *check = 1;
+            *contador += 1;
+        }
+        else
+            break;
+    }
+
+    free (contador);
 }
 
-void mostrarEntrada (char ** numero) {
+void mostrarEntrada (char * numero , short* baseOrigen , short* baseDestino , short* verbose , short* help) {
+    printf("La entrada es: \n");
+    printf("Numero : %s\n",numero);
+    printf("Base origen : %hd\n",*baseOrigen);
+    printf("Base destino : %hd\n",*baseDestino);
+    printf("Verbose mode : ");
+    if(*verbose == 1){
+        printf("Encendido\n");
+    }
+    else{
+        printf("Apagado\n");
+    }
+    printf("Help : ");
+    if(*help == 1){
+        printf("Encendido\n\n");
+    }
+    else{
+        printf("Apagado\n\n");
+    }
+}
 
+void mostrarHelp(){
+    printf("MENU DE AYUDA:\nEl programa recibe las siguientes entradas. No importa el orden.\n");
+    printf("-n	Indica que la siguiente entrada es el numero a convertir. Es obligatorio.<number> Es el numero que le sigue a -n.\n");
+    printf("Asegurese de lo siguiente:\n");
+    printf("* Que el numero sea valido en su base de origen.\n");
+    printf("* Que la parte entera no posea más de 10 dígitos.\n");
+    printf("* Que la parte fraccionaria no posea más de 6 dígitos.\n");
+    printf("Es obligatorio para el correcto funcionamiento.\n\n");
+
+    printf("-s	Indica que la siguiente entrada es la base origen del numero ingresado.\n");
+    printf("Las bases validas se comprenden entre el siguiente rango: [2..16]\n");
+    printf("Si utiliza '-s', debe ingresar una base correcta <number> en el siguiente argumento.\n");
+    printf("Si no lo utiliza, el programa utiliza de forma predeterminada base 10.\n");
+
+    printf("-d	Indica que la siguiente entrada es la base destino del numero ingresado.\n");
+    printf("Las bases validas se comprenden entre el siguiente rango: [2..16]\n");
+    printf("Si utiliza '-d', debe ingresar una base correcta <number> en el siguiente argumento.\n");
+    printf("Si no lo utiliza, el programa utiliza de forma predeterminada base 10.\n");
+
+    printf("-v	Indica al programa que se debe ejecutar en modo 'verbose', mostrando los calculos intermedios del proceso de conversion.\n");
+
+    printf("-h	Indica al programa que se debe mostrar el menu de ayuda.\n");
+
+    printf("Para informacion adicional acerca del funcionamiento del programa, consultar el informe especifico.\n\n");
 }
 
 int main (int argc, char ** argv) {
@@ -49,14 +106,12 @@ int main (int argc, char ** argv) {
     printf ("\n");
     printf ("Nombre del programa = %s", argv [0]);
     printf ("\n");
-    mostrarEntrada (argv);
 
     if (argc >= 2 && argc <= 9) {
 
         while (*contador < argc) {
 
             if (strcmp (argv [*contador], "-n" ) == 0) {
-                // printf ("Asigne -n");
                 *contador += 1;
                 validarNumero (argv [*contador], check);
 
@@ -70,7 +125,6 @@ int main (int argc, char ** argv) {
             }
 
             else if (strcmp (argv [*contador], "-s") == 0) {
-                // printf ("Asigne -s");
                 *contador += 1;
                 validarBase (argv [*contador], check);
 
@@ -84,7 +138,6 @@ int main (int argc, char ** argv) {
             }
 
             else if (strcmp (argv [*contador], "-d") == 0 ) {
-                // printf ("Asigne -d");
                 *contador += 1;
                 validarBase (argv [*contador], check);
 
@@ -98,12 +151,10 @@ int main (int argc, char ** argv) {
             }
 
             else if (strcmp (argv [*contador], "-v") == 0 ) {
-                // printf ("Asigne -v");
                 *verbose = 1;
             }
 
             else if (strcmp (argv [*contador], "-h") == 0 ) {
-                // printf ("Asigne -h");
                 *help = 1;
             }
 
@@ -115,9 +166,12 @@ int main (int argc, char ** argv) {
             *check = 0;
             *contador += 1;
         }
-
+        mostrarEntrada(numero,baseOrigen,baseDestino,verbose,help);
+        if( *help == 1 ){
+            mostrarHelp();
+        }
         if (numero == NULL) {
-            printf ("Error: No se ingreso un numero. \n");
+            printf ("No se ingreso un numero. \n");
             *procesoCompleto = 0;
         }
         else if (*procesoCompleto == 0) {
